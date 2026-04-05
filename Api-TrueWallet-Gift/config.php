173c6995aa3af0d6c74f8b49061af703e54bcef7
NOZEED <133743911+Nozeed@users.noreply.github.com>
2026-04-05 20:06:57 +0700

    Update config.php

--- Api-TrueWallet-Gift/config.php
+++ Api-TrueWallet-Gift/config.php
@@ -6,14 +6,9 @@ $tmweasy["user"] = ""; //Username ใช้ล็อกอินเว็บ htt
 $tmweasy["password"] = ""; //Password ใช้ล็อกอินเว็บ https://www.tmweasyapi.com/login.php
 $truewallet["mobile"] = ""; //เบอร์ทรูวอเลท ตัวเลขเท่านั้น
 $truewallet["name"] = ""; //ฃื่อบัญชีวอเลท
-
-
 $database_set["truewallet"]["mul"] = 1; //ตัวคูณเครดิตรสำหรับทรูวอเลท และซองของขวีญ
-
-
-$database_set["database_type"] = 2; //ตั้งค่าชนิด ฐานข้อมูล 0=ไม่เชื่อมฐานข้อมูล พัฒนาเพิ่มเอง | 1=mysql | 2=mysqli | 3=mssql | 4=odbc | 5=sqlsrv
-$database_set["host"] = "localhost"; // Host ฐานข้อมูล
-$database_set["user"] = "root"; // User ฐานข้อมูล
+$database_set["host"] = ""; // Host ฐานข้อมูล
+$database_set["user"] = ""; // User ฐานข้อมูล
 $database_set["password"] = ""; // Password ฐานข้อมูล
 $database_set["db_name"] = ""; // ชื่อฐานข้อมูล
 $database_set["user_table"] = ""; // ชื่อตารางผู้ใช้
