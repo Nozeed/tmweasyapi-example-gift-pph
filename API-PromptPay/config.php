701492b4deb1c0134dd98ee1c0f41a1d6240d63c
NOZEED <133743911+Nozeed@users.noreply.github.com>
2026-04-19 22:36:56 +0700

    Refactor configuration variables for PromptPay API

--- API-PromptPay/config.php
+++ API-PromptPay/config.php
@@ -1,20 +1,20 @@
 <?php
-$api_url = "https://www.tmweasy.com/api_pph.php"; //หรือสำรอง https://tmwallet.thaighost.net/api_pph.php
-$tmweasy_user = ""; //Username ใช้ล็อกอินเว็บ https://www.tmweasyapi.com/login.php
-$tmweasy_password = ""; //Password ใช้ล็อกอินเว็บ https://www.tmweasyapi.com/login.php
-$tmweasy_api_key = ""; //api key ที่ได้จากการเปิดใช้งาน Qr Promptpay Api บนเว็บ  tmweasy
+$api_url = "https://tmwallet.thaighost.net/api_pph.php";
+$tmweasy["user"] = ""; //Username ใช้ล็อกอินเว็บ https://www.tmweasyapi.com/login.php
+$tmweasy["password"] = ""; //Password ใช้ล็อกอินเว็บ https://www.tmweasyapi.com/login.php
+$tmweasy["api_key"] = ""; //api key ที่ได้จากการเปิดใช้งาน Qr Promptpay Api บนเว็บ  tmweasy
 
-$con_id = ""; //conid ที่ได้จากการเปิดใช้งาน Qr Promptpay Api บนเว็บ  tmweasy
-$prommpay_no = "";  //เลข ID พร้อมเพย์ ใส่เฉพาะตัวเลขเช่น เบอร์โทร เลขบัตร ปชช 
-$prommpay_type = "01"; //ประเพทพร้อมเพย์  01 = Mobile, 02 = เลขบัตร ปชช, 03 = E-Wallet
-$prommpay_name = ""; //ชื่อและนามสกุล
+$promptpay["con_id"] = ""; //conid ที่ได้จากการเปิดใช้งาน Qr Promptpay Api บนเว็บ  tmweasy
+$promptpay["no"] = "";  //เลข ID พร้อมเพย์ ใส่เฉพาะตัวเลขเช่น เบอร์โทร เลขบัตร ปชช 
+$promptpay["type"] = "01"; //ประเพทพร้อมเพย์  01 = Mobile, 02 = เลขบัตร ปชช, 03 = E-Wallet
+$promptpay["name"] = ""; //ชื่อและนามสกุล
 
 //--------------- การเชื่อม ฐานข้อมูล เพื่ออัพเดทเครดิตรให้ลูกค้า----------------
-$database_host = "localhost";
-$database_user = "root";
-$database_password = "";
-$database_db_name = "";
+$database_set["host"] = "localhost"; // Host ฐานข้อมูล
+$database_set["user"] = "root"; // User ฐานข้อมูล
+$database_set["password"] = ""; // Password ฐานข้อมูล
+$database_set["db_name"] = ""; // ชื่อฐานข้อมูล
 
-$database_table = ""; //ตารางที่เต็มข้อมูลลูกค้า หรือ เก็บข้อมูลเครดิตร
-$database_user_field = ""; //ฟิวที่ใช้ในการอ้างอิง user เช่น username userid
-$database_point_field = ""; //ฟิวที่ใช้ในการเก็บค่า พ้อย เครดิตร ที่ต้องการให้อัพเดทหลังเต็มเสร็จ
+$database_set["table"] = ""; //ชื่อตารางผู้ใช้
+$database_set["user_field"] = ""; //ชื่อฟิลด์ชื่อผู้ใช้
+$database_set["point_field"] = ""; //ชื่อฟิลด์เครดิต
