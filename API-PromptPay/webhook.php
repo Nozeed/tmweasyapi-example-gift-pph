141410c394ce8491245a90cb30a511a2c892bc1f
NOZEED <133743911+Nozeed@users.noreply.github.com>
2026-04-19 22:38:36 +0700

    Update webhook.php

--- API-PromptPay/webhook.php
+++ API-PromptPay/webhook.php
@@ -8,7 +8,7 @@ include("config.php");
 
 // Direct PDO connection
 try {
-	$conn = new PDO("mysql:host=$database_host;dbname=$database_db_name;charset=utf8mb4", $database_user, $database_password);
+	$conn = new PDO("mysql:host={$database_set["host"]};dbname={$database_set["db_name"]};charset=utf8mb4", $database_set["user"], $database_set["password"]);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	die("Error PDO Database is not connect: " . $e->getMessage());
@@ -18,12 +18,12 @@ if ($_POST["data"]) {
 	$data_api = json_decode($_POST["data"], true);
 	$ts = time();
 	if (($ts - $data_api["timestamp"]) < 30) {
-		$signature = md5($_POST["data"] . ":" . $tmweasy_api_key);
+		$signature = md5($_POST["data"] . ":" . $tmweasy["api_key"]);
 		if ($signature == $_POST["signature"]) {
 			//---------------------------------อัพเดทฐานข้อมูลเครดิตรลูกค้่า เมื่อเติมสำเร็จ---------------------------------------------------------------------------------------------------
 			$point = $data_api["amount"];
 			$ref1 = $data_api["ref1"];
-			$stmt = $conn->prepare("update $database_table set $database_point_field = $database_point_field + :point where $database_user_field = :ref1");
+			$stmt = $conn->prepare("update {$database_set["table"]} set {$database_set["point_field"]} = {$database_set["point_field"]} + :point where {$database_set["user_field"]} = :ref1");
 			$stmt->bindParam(":point", $point);
 			$stmt->bindParam(":ref1", $ref1);
 			$stmt->execute();
